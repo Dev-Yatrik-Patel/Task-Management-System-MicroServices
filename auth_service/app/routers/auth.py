ff99b0f7e8f9b68139e0d629d81359ff9deaf71f
Dev-Yatrik-Patel <yatrik.patel@openxcell.com>
2026-02-10 10:16:10 +0530

    Refactor: service layers to use service classes so that domain logic, authorization, and persistence are encapsulated per domain

--- auth_service/app/routers/auth.py
+++ auth_service/app/routers/auth.py
@@ -12,25 +12,23 @@ from app.schemas.auth import (
     TokenValidationRequest, 
     TokenValidationResponse, 
     RegisterResponse )
-from app.services.auth_service import (
-    register_user,
-    authenticate_user,
-    validate_token
-)
+from app.services.auth_service import AuthService
 
 
 router = APIRouter(prefix="/auth", tags=["Authentication"])
 
 @router.post("/register", status_code=201)
 async def register(payload: RegisterRequest, db: Session = Depends(get_db)):
-    user = register_user(db, payload.email, payload.password)
+    service = AuthService(db)
+    user = service.register_user(payload.email, payload.password)
     data = RegisterResponse.model_validate(user).model_dump(mode="json")
     return success_response(data = data, status_code = status.HTTP_201_CREATED, message = "User registered successfully.")
 
 
 @router.post("/login")
 async def login(payload: LoginRequest, db: Session = Depends(get_db)):
-    token = authenticate_user(db, payload.email, payload.password)
+    service = AuthService(db)
+    token = service.authenticate_user(payload.email, payload.password)
     token_response_model = TokenResponse(access_token = token)
     data = TokenResponse.model_validate(token_response_model).model_dump(mode="json")
     return success_response(data = data, status_code = status.HTTP_200_OK, message = "User logged-in successfully.")
@@ -38,7 +36,7 @@ async def login(payload: LoginRequest, db: Session = Depends(get_db)):
 
 @router.post("/validate-token")
 async def validate(payload: TokenValidationRequest):
-    validated_payload = validate_token(payload.token)
+    validated_payload = AuthService.validate_token(payload.token)
     tokenvalidate_response = TokenValidationResponse(auth_user_id= int(validated_payload["sub"])
                                                      ,email= validated_payload["email"]
                                                      ,is_active= bool(validated_payload["is_active"])
--- auth_service/app/services/auth_service.py
+++ auth_service/app/services/auth_service.py
@@ -9,43 +9,48 @@ from app.core.security import (
     decode_token,
 )
 
+class AuthService:
+    
+    def __init__(self, db: Session):
+        self.db = db
 
-def register_user(db: Session, email: str, password: str) -> User:
-    existing_user = db.query(User).filter(User.email == email).first()
-    if existing_user:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Email already registered"
+    def register_user(self, email: str, password: str) -> User:
+        existing_user = self.db.query(User).filter(User.email == email).first()
+        if existing_user:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="Email already registered"
+            )
+
+        user = User(
+            email=email,
+            password_hash=hash_password(password)
         )
+        self.db.add(user)
+        self.db.commit()
+        self.db.refresh(user)
+        return user
 
-    user = User(
-        email=email,
-        password_hash=hash_password(password)
-    )
-    db.add(user)
-    db.commit()
-    db.refresh(user)
-    return user
+    def authenticate_user(self, email: str, password: str) -> str:
+        user = self.db.query(User).filter(User.email == email).first()
+        if not user or not verify_password(password, user.password_hash):
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Invalid credentials"
+            )
 
-def authenticate_user(db: Session, email: str, password: str) -> str:
-    user = db.query(User).filter(User.email == email).first()
-    if not user or not verify_password(password, user.password_hash):
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Invalid credentials"
+        token = create_access_token(
+            data={"sub": str(user.id), "email": user.email, "is_active" : str(user.is_active), "created_at": str(user.created_at)}
         )
-
-    token = create_access_token(
-        data={"sub": str(user.id), "email": user.email, "is_active" : str(user.is_active), "created_at": str(user.created_at)}
-    )
-    return token
-
-def validate_token(token: str) -> dict:
-    try:
-        payload = decode_token(token)
-        return payload
-    except Exception:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Invalid or expired token"
-        )
+        return token
+    
+    @staticmethod
+    def validate_token(token: str) -> dict:
+        try:
+            payload = decode_token(token)
+            return payload
+        except Exception:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Invalid or expired token"
+            )
